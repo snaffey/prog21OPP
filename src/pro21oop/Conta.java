b1ce6a0c3bfafc2cdfd1bf1d247e5719759dc40c
Tiago <ggtiago0santos@gmail.com>
2022-10-10 23:14:08 +0100

    starting

---
+++ src/pro21oop/Conta.java
@@ -0,0 +1,18 @@
+package pro21oop;
+public class Conta {
+    /*
+    Atributos da classe/modelo
+    Por padrão o encapsulamento é publico
+    */
+    int numero;//por padrão é igual public int numero; 
+    String nomeTitular;
+    double saldo;
+    //função sem retorno
+    void levantamento(double qt){
+        double novoSaldo = this.saldo-qt;
+        this.saldo = novoSaldo;
+    }
+    void deposito(double qt){
+        this.saldo+=qt;
+    }
+}
