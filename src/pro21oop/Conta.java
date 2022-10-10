b144a55dd34f1aaa5f4c34fd889d64960d4be52d
Tiago <ggtiago0santos@gmail.com>
2022-10-10 23:18:59 +0100

    Fix

--- src/pro21oop/Conta.java
+++ src/pro21oop/Conta.java
@@ -1,18 +1,21 @@
 package pro21oop;
+
 public class Conta {
     /*
-    Atributos da classe/modelo
-    Por padrão o encapsulamento é publico
-    */
-    int numero;//por padrão é igual public int numero; 
+     * Atributos da classe/modelo
+     * Por padrão o encapsulamento é publico
+     */
+    int numero; // por padrão é igual public int numero;
     String nomeTitular;
     double saldo;
-    //função sem retorno
-    void levantamento(double qt){
-        double novoSaldo = this.saldo-qt;
+
+    // função sem retorno
+    void levantamento(double qt) {
+        double novoSaldo = this.saldo - qt;
         this.saldo = novoSaldo;
     }
-    void deposito(double qt){
-        this.saldo+=qt;
+
+    void deposito(double qt) {
+        this.saldo += qt;
     }
 }
--- src/pro21oop/Pro21OOP.java
+++ src/pro21oop/Pro21OOP.java
@@ -3,22 +3,24 @@ package pro21oop;
 import java.util.Scanner;
 
 public class Pro21OOP {
-    
+
     public static void main(String[] args) {
-        /*System.out.println("Ola");
-        Scanner entrada = new Scanner(System.in);
-        int idade = entrada.nextInt();
-        System.out.println("idade "+idade);*/
-        
+        /*
+         * System.out.println("Ola");
+         * Scanner entrada = new Scanner(System.in);
+         * int idade = entrada.nextInt();
+         * System.out.println("idade "+idade);
+         */
+
         Conta conta = new Conta();
         conta.numero = 1;
-        System.out.println("num da conta -> "+conta.numero);
-        
+        System.out.println("num da conta -> " + conta.numero);
+
         conta.saldo = 1000;
-        System.out.println("Saldo da conta -> "+conta.saldo);
-        conta.levantamento(100);        
-        System.out.println("Saldo da conta depois -> "+conta.saldo);
-        
+        System.out.println("Saldo da conta -> " + conta.saldo);
+        conta.levantamento(100);
+        System.out.println("Saldo da conta depois -> " + conta.saldo);
+
     }
-    
+
 }
